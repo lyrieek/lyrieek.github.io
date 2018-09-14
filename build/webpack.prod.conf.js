a48d79d6004ba47ae6cf78812b7718e1664e00e2
lyrieek <tianhao7@foxmail.com>
2018-09-14 12:08:11 +0800

    improve build config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,153 +4,143 @@ const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
 const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
+const baseConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : require('../config/prod.env')
-
-const webpackConfig = merge(baseWebpackConfig, {
-  module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.build.productionSourceMap,
-      extract: true,
-      usePostCSS: true
-    })
-  },
-  devtool: config.build.productionSourceMap ? config.build.devtool : false,
-  output: {
-    path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': env
-    }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false
-        }
-      },
-      sourceMap: config.build.productionSourceMap,
-      parallel: true
-    }),
-    new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css'),
-      allChunks: true
-    }),
-    new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap
-        ? { safe: true, map: { inline: false } }
-        : { safe: true }
-    }),
-    new HtmlWebpackPlugin({
-      filename: config.build.index,
-      template: 'index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-      },
-      chunksSortMode: 'dependency'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'color.html',
-      template: 'color.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        minifyCSS: true,
-        minifyJS: true
-      },
-      chunks: [],
-      chunksSortMode: 'dependency'
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks (module) {
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
-  ]
+const webpackConfig = merge(baseConfig, {
+	module: {
+		rules: utils.styleLoaders({
+			sourceMap: config.build.productionSourceMap,
+			extract: true,
+			usePostCSS: true
+		})
+	},
+	devtool: config.build.productionSourceMap ? config.build.devtool : false,
+	output: {
+		path: config.build.assetsRoot,
+		filename: utils.assetsPath('js/[name].[chunkhash].js'),
+		chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: 'production'
+			}
+		}),
+		new UglifyJsPlugin({
+			uglifyOptions: {
+				compress: {
+					warnings: false
+				}
+			},
+			sourceMap: config.build.productionSourceMap,
+			parallel: true
+		}),
+		new ExtractTextPlugin({
+			filename: utils.assetsPath('css/[name].[contenthash].css'),
+			allChunks: true
+		}),
+		new OptimizeCSSPlugin({
+			cssProcessorOptions: config.build.productionSourceMap
+				? {
+					safe: true,
+					map: {
+						inline: false
+					}
+				}
+				: {
+					safe: true
+				}
+		}),
+		new HtmlWebpackPlugin({
+			filename: config.build.index,
+			template: 'index.html',
+			inject: true,
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true,
+				minifyCSS: true,
+				minifyJS: true
+			},
+			chunksSortMode: 'dependency'
+		}),
+		new webpack.HashedModuleIdsPlugin(),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks(module) {
+				return (
+					module.resource && /\.js$/.test(module.resource) && module.resource.indexOf(
+						path.join(__dirname, '../node_modules')
+					) === 0
+				)
+			}
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'manifest',
+			minChunks: Infinity
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'app',
+			async: 'vendor-async',
+			children: true,
+			minChunks: 3
+		}),
+		new CleanWebpackPlugin(utils.resolve('dist'), {
+			root: utils.resolve()
+		}),
+		new CopyWebpackPlugin([{
+			from: utils.resolve('static'),
+			to: config.build.assetsSubDirectory,
+			ignore: ['.*']
+		}])
+	]
 });
 
-['color', 'font-icon'].map((fileName) => {
-  webpackConfig.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: fileName + '.html',
-      template: fileName + '.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        minifyCSS: true,
-        minifyJS: true
-      },
-      chunks: [],
-      chunksSortMode: 'dependency'
-    })
-  )
-});
+['color', 'font-icon'].map((fileName) =>
+	webpackConfig.plugins.push(
+		new HtmlWebpackPlugin({
+			filename: fileName + '.html',
+			template: fileName + '.html',
+			inject: true,
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true,
+				minifyCSS: true,
+				minifyJS: true
+			},
+			chunks: [],
+			chunksSortMode: 'dependency'
+		})
+	));
 
 
 if (config.build.productionGzip) {
-  const CompressionWebpackPlugin = require('compression-webpack-plugin')
+	const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
-      ),
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  )
+	webpackConfig.plugins.push(
+		new CompressionWebpackPlugin({
+			asset: '[path].gz[query]',
+			algorithm: 'gzip',
+			test: new RegExp(
+				'\\.(' + config.build.productionGzipExtensions.join('|') + ')$'
+			),
+			threshold: 10240,
+			minRatio: 0.8
+		})
+	)
 }
 
 if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+	const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+	webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
 module.exports = webpackConfig
