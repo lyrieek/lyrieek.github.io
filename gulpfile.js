373bb31a670479d2e86c72a74e445bf0f73b1f1c
lyrieek <tianhao7@foxmail.com>
2018-10-18 16:05:06 +0800

    update .eslintrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,23 @@
 const gulp = require('gulp')
-const gutil = require('gulp-util')
-const uglify = require('gulp-uglify')
+// const gutil = require('gulp-util')
 const minify = require('gulp-minify')
-const useref = require('gulp-useref')
-const fse = require("fs-extra")
-const sourcemaps = require('gulp-sourcemaps')
-const combiner = require('stream-combiner2')
-const _ = require('lodash');
+const useref = require('gulp-useref');
 
 (function() {
-  gulp.task('main', function() {
-    gulp.src('src/common-1.0.js')
-      .pipe(minify())
-      .pipe(useref())
-      .pipe(gulp.dest('dist'))
-  });
-  gulp.task('default', ['main']);
-  const handleError = function(err) {
-    const colors = gutil.colors;
-    console.log('\n')
-    gutil.log(colors.red('Error!'))
-    gutil.log('fileName: ' + colors.red(err.fileName))
-    gutil.log('lineNumber: ' + colors.red(err.lineNumber))
-    gutil.log('message: ' + err.message)
-    gutil.log('plugin: ' + colors.yellow(err.plugin))
-  }
+	gulp.task('main', function() {
+		gulp.src('src/common-1.0.js')
+			.pipe(minify())
+			.pipe(useref())
+			.pipe(gulp.dest('dist'))
+	});
+	gulp.task('default', ['main']);
+	// const handleError = function(err) {
+	// 	const colors = gutil.colors;
+	// 	console.log('\n')
+	// 	gutil.log(colors.red('Error!'))
+	// 	gutil.log('fileName: ' + colors.red(err.fileName))
+	// 	gutil.log('lineNumber: ' + colors.red(err.lineNumber))
+	// 	gutil.log('message: ' + err.message)
+	// 	gutil.log('plugin: ' + colors.yellow(err.plugin))
+	// }
 })();
