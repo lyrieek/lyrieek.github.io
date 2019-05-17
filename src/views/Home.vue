7b42878593b36f82955a2d42b94c0e127726317d
lyrieek <tianhao7@foxmail.com>
2019-05-17 14:45:07 +0800

    adjustment project config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
 		<div class="header-container">
 			<h1 style="text-shadow: 5px 5px #1177aa;">快速,全面,丰富的前端帮助文档</h1>
 			<div>
-				<pre>我将分批次开发文档，第一批包括CSS3 & HTML5 & ES6，第二批包括NPM & Webpack & Gulp，第二批包括Vue & Angular & React</pre>
+				<pre>我将分批次开发文档,第一批包括CSS3 & HTML5 & ES6,第二批包括NPM & Webpack & Gulp,第二批包括Vue & Angular & React</pre>
 			</div>
 		</div>
 		<hr>
@@ -12,19 +12,19 @@
 			<el-col :span="8">
 				<div class="grid-content bg-purple">
 					<h2>内容丰富</h2>
-					<span>这里不但有官方详细的内容与翻译，还有对应的问题与解答，并且扩充了大量举例，满足各种需要</span>
+					<span>这里不但有官方详细的内容与翻译,还有对应的问题与解答,并且扩充了大量举例,满足各种需要</span>
 				</div>
 			</el-col>
 			<el-col :span="8">
 				<div class="grid-content bg-purple">
-					<h2>高可信度</h2>
-					<span>每段内容都会给出原文链接的位置，站长自任小白鼠，测试大多数错误内容，避开你将要跳下去的坑</span>
+					<h2>维护轻便</h2>
+					<span>T-Doc使用Vue + Element-UI构建,配置轻巧,便于改动,并在vue-cli的配置基础上进行了一些整合</span>
 				</div>
 			</el-col>
 			<el-col :span="8">
 				<div class="grid-content bg-purple">
 					<h2>频繁维护</h2>
-					<span>定期自动检查并更进官网更新，站长审核后会做出相应更新，不过这一点暂时没有做到</span>
+					<span>定期自动检查并更进官网更新,更进最新文档进行更新,不过这一点暂时没有做到</span>
 				</div>
 			</el-col>
 		</el-row>
