069e2b91641407c009f9eaaaa54d56405c712f48
lyrieek <tianhao7@foxmail.com>
2019-05-21 15:30:38 +0800

    :x: remove check-versions.js

--- build/build.js
+++ build/build.js
@@ -9,6 +9,57 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('./config')
 const webpackConfig = require('./webpack.prod.conf')
+const semver = require('semver')
+const packageConfig = require('../package.json')
+const shell = require('shelljs')
+
+function exec(cmd) {
+	return require('child_process').execSync(cmd).toString().trim()
+}
+
+const versionRequirements = [{
+	name: 'node',
+	currentVersion: semver.clean(process.version),
+	versionRequirement: packageConfig.engines.node
+}]
+
+if (shell.which('npm')) {
+	versionRequirements.push({
+		name: 'npm',
+		currentVersion: exec('npm --version'),
+		versionRequirement: packageConfig.engines.npm
+	})
+}
+
+module.exports = function() {
+	const warnings = []
+
+	for (let i = 0; i < versionRequirements.length; i++) {
+		const mod = versionRequirements[i]
+
+		if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
+			warnings.push(mod.name + ': '
+				+ chalk.red(mod.currentVersion) + ' should be '
+				+ chalk.green(mod.versionRequirement)
+			)
+		}
+	}
+
+	if (warnings.length) {
+		console.log('')
+		console.log(chalk.yellow('To use this template, you must update following to modules:'))
+		console.log()
+
+		for (let i = 0; i < warnings.length; i++) {
+			const warning = warnings[i]
+			console.log('  ' + warning)
+		}
+
+		console.log()
+		process.exit(1)
+	}
+}
+
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (err) => {
 	if (err) throw err
